dda1b0e353051796527a6cb58edbf3719fa82d2f
ashish23072002 <ashish23bisht@gmail.com>
2025-07-29 17:43:34 +0530

    SettingUp Vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-amber-100 text-shadow-amber-700 text-7xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
